8298212426bf22f3498102848005dd47aa1dd249
lucaslrp <lucaslrpassos@gmail.com>
2023-11-28 23:07:53 -0300

    first commit

---
+++ cypress/e2e/tasks.cy.js
@@ -0,0 +1,95 @@
+/// <reference types="cypress"/>
+
+//const { method } = require("cypress/types/bluebird")
+
+//import {faker} from '@faker-js/faker'
+
+describe('tasks', () => {
+
+    let testData;
+
+    before(() => {
+        cy.fixture('tasks').then(t => {
+            testData = t
+        })
+    })
+
+    context('cadastro', () => {
+
+        it('deve cadastrar uma nova tarefa', () => {
+
+            const taskName = 'Ler um livro de node.js'
+
+            cy.removeTaskByName(taskName)
+            cy.createTask(taskName)
+            cy.contains('main div p', taskName)
+                .should('be.visible')
+        })
+
+        it('não deve permitir tarefa duplicada', () => {
+
+            const task = testData.dup
+
+            cy.removeTaskByName(task.name)
+            cy.postTask(task)
+            cy.createTask(task.name)
+
+            cy.get('.swal2-html-container')
+                .should('be.visible')
+                .should('have.text', 'Task already exists!')
+
+        })
+
+        it('campo obrigatório', () => {
+
+            cy.createTask()
+            cy.isRequired('This is a required field')
+
+        })
+    })
+    context('atualização', () => {
+
+        it('deve concluir tarefa', () => {
+
+            const task = {
+                name: 'Pagar contas de consumo',
+                is_done: false
+            }
+
+            cy.removeTaskByName(task.name)
+            cy.postTask(task)
+            cy.visit('/')
+            cy.contains('p', task.name)
+                .parent()
+                .find('button[class*=ItemToggle]')
+                .click()
+            cy.contains('p', task.name)
+                .should('have.css', 'text-decoration-line', 'line-through')
+
+        })
+
+    })
+
+    context('exclusão', () => {
+
+        it('deve remover uma tarefa', () => {
+
+            const task = {
+                name: 'Estudar Javascript',
+                is_done: false
+            }
+
+            cy.removeTaskByName(task.name)
+            cy.postTask(task)
+            cy.visit('/')
+            cy.contains('p', task.name)
+                .parent()
+                .find('button[class*=ItemDelete]')
+                .click()
+            cy.contains('p', task.name)
+                .should('not.exist')
+
+        })
+
+    })
+})
